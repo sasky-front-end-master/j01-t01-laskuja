49804eb7d41a184688a15965e8698aee492876ea
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2020-09-16 13:23:34 +0300

    testausmateriaali päivitetty

--- tehtava/tests/tehtava.test.js
+++ tehtava/tests/tehtava.test.js
@@ -4,15 +4,15 @@ global.console = {
 }
 
 describe('Tarkistetaan console.login sisältö', () => {
-  it('pitäisi sisältää Nyt koodataan -teksti', () => {
+  it('pitäisi sisältää laskut', () => {
     
     const tehtava = require('../tehtava.js');
-
     tehtava();
 
-    expect(global.console.log).toHaveBeenCalledWith(
-      'Nyt koodataan!'
-    )
+    expect(global.console.log).toHaveBeenCalledWith('6+2=8');
+    expect(global.console.log).toHaveBeenCalledWith('6-2=4');
+    expect(global.console.log).toHaveBeenCalledWith('6*2=12');
+    expect(global.console.log).toHaveBeenCalledWith('6/2=3');
   });
 });
 
