54393805dd75c759752bb39ef442765901157358
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2020-09-16 13:17:14 +0300

    tehtävän määritys

---
+++ README.md
@@ -0,0 +1,22 @@
+# Laskuja
+
+  Sijoita muuttujiin seuraavat luvut:
+  * luku1 = 6
+  * luku2 = 2
+  
+  Laske ohjelmassa luvuista summa, erotus, osamäärä ja
+  tulo. Tulosta laskut ja toimenpiteet. Tulosta tekstit console.log -metodin avulla.
+
+  Malliesimerkki ohjelman tulosteesta:
+  ```
+    6+2=8
+    6-2=4
+    6*2=12
+    6/2=3
+  ```
+ 
+  Ohjelman voit suorittaa komentorivillä komennolla:
+  ```
+    npm start
+  ```
+
---
+++ tehtava/tehtava.js
@@ -0,0 +1,23 @@
+
+function tehtava() {
+
+  // Sijoita muuttujiin seuraavat luvut:
+  // luku1 = 6
+  // luku2 = 2
+  //
+  // Laske ohjelmassa luvuista summa, erotus, osamäärä ja
+  // tulo. Tulosta laskut ja toimenpiteet. Tulosta tekstit
+  // console.log -metodin avulla.
+  //
+  // Malliesimerkki ohjelman tulosteesta:
+  //  6+2=8
+  //  6-2=4
+  //  6*2=12
+  //  6/2=3
+
+  let out = 'tänne laskuja';
+  console.log(out);
+
+}
+
+module.exports = tehtava;
